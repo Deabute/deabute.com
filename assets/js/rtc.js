1af9e623a58372a992ee1eb09d3b8dff8373c41c
PaulBeaudet <inof8or@gmail.com>
2019-01-11 16:15:40 -0500

    clean up, add comments, make the interface make some sense

--- assets/js/rtc.js
+++ assets/js/rtc.js
@@ -1,113 +1,76 @@
 // rtc.js ~ copyright 2019 Paul Beaudet ~ MIT License
-
-var rtc = {
-    config: {
-        'iceServers': [
-            {'urls': 'stun:stun.stunprotocol.org:3478'},
-            {'urls': 'stun:stun.l.google.com:19302'}
-        ]
-    },
-    server: null,
-    peer: null,
-    remoteConnection: null,
-    dataChannel: null,
-    localID: {
-        sdp: null,
-        ice: []
-    },
+// This test requires at least two browser windows, to open a data connection between two peers
+var rtc = { // stun servers in config allow client to introspect a communication path to offer a remote peer
+    config: {'iceServers': [ {'urls': 'stun:stun.stunprotocol.org:3478'}, {'urls': 'stun:stun.l.google.com:19302'} ]},
+    peer: null,                      // placeholder for parent webRTC object instance
+    dataChannel: null,               // object for local data channel end point
+    localID: { sdp: null, ice: [] }, // package for user to send off to a friend they want to connect with
+    channelOpen: false,              // false: closed data channel, true: open data channel
     connectInit: function(){
-        rtc.peer = new RTCPeerConnection(rtc.config);
-        rtc.dataChannel = rtc.peer.createDataChannel('chat');
-        rtc.peer.onicecandidate = function onIce(event) { // on address info being introspected
-            if(event.candidate){
-                console.log('got a candidate ice');
-                rtc.localID.ice.push(JSON.stringify(event.candidate));
-            } else {
-                var stringifiedID = JSON.stringify(rtc.localID);
-                document.getElementById('friendAddress').value = stringifiedID;
-            }
-        };
-        rtc.peer.ondatachannel = rtc.newDataChannel;
+        rtc.peer = new RTCPeerConnection(rtc.config);           // create new instance for local client
+        rtc.dataChannel = rtc.peer.createDataChannel('chat');   // Creates data endpoint for client's side of connection
+        rtc.peer.onicecandidate = function onIce(event) {       // on address info being introspected (after local discription is set)
+            if(event.candidate){                                // canididate property denotes data as multiple canidates can resolve
+                rtc.localID.ice.push(event.candidate);          // Add a canidate to an array that we can package up and show user
+            } else {                                            // null event.canidate means we finished recieving canidates
+                app.resultsField.innerHTML = JSON.stringify(rtc.localID); // show user info to share with friend
+            } // NOTE we show connection info to share once ice canidates are complete this can be slower to find a route
+        };    // Also note that sdp is going to be negotiated first regardless of any media being involved. its faster to resolve
+        rtc.peer.ondatachannel = rtc.newDataChannel;            // creates data endpoints for remote peer on rtc connection
     },
-    instigateConnection: function(){ // return offer to client so they can send it to remote
-        rtc.peer.createOffer().then( function onOffer(desc){
-            return rtc.peer.setLocalDescription(desc);
+    getOffer: function(){                                       // extend offer to client so they can send it to remote
+        rtc.peer.createOffer().then( function onOffer(desc){    // get sdp data to show user, that will share with a friend
+            return rtc.peer.setLocalDescription(desc);          // note what sdp data self will use
         }).then( function onSet(){
-            rtc.localID.sdp = JSON.stringify(rtc.peer.localDescription);
-        }).catch(console.log);
+            rtc.localID.sdp = rtc.peer.localDescription;        // set local discription into something that can be shared
+        });
     },
-    handleRemoteID: function(){
-        var id = JSON.parse(document.getElementById('friendAddress').value);
-        var sdp = JSON.parse(id.sdp);
-        rtc.peer.setRemoteDescription(sdp).catch(console.log);
+    offersAndAnswers: function(){                               // method that gets called in recieving case with shared data provided
+        var id = JSON.parse(app.connectInput.value);            // should be provided sdp and ice canidates from remote peer
+        rtc.peer.setRemoteDescription(id.sdp);
         for(var i = 0; i < id.ice.length; i++){
-            rtc.peer.addIceCandidate(JSON.parse(id.ice[i])).catch(console.log);
+            rtc.peer.addIceCandidate(id.ice[i]);
         }
-
-        if(sdp.type === 'answer'){
-            console.log('recieved answer');
-        } else {
-            rtc.peer.createAnswer().then(function(answer){
+        if(id.sdp.type === 'offer'){                            // create an answer when type is offer
+            rtc.peer.createAnswer().then(function onAnswer(answer){
                 return rtc.peer.setLocalDescription(answer);
-            }).then(
-                function(){
-                    console.log('set local description');
-                    rtc.localID.sdp = JSON.stringify(rtc.peer.localDescription);
-                    document.getElementById('friendAddress').value = JSON.stringify(rtc.localID);
-                }
-            );
+            }).then(function onOfferSetDesc(){
+                rtc.localID.sdp = rtc.peer.localDescription;
+            });
         }
     },
-    openDataChannel: function(){
-        rtc.dataChannel.send('what the fuck is happening');
-        console.log('channel open');
-    },
-    closeDataChannel: function(){
-        console.log('channel closed');
-    },
-    onMsg: function(event){
-        console.log(event.data);
-    },
-    sendData: function(msg){
-        rtc.dataChannel.send(msg);
-    },
+    sendData: function(msg){if(rtc.channelOpen){rtc.dataChannel.send(msg);}},
     newDataChannel: function(event){
         receiveChannel = event.channel;
-        receiveChannel.onerror = console.log;
-        receiveChannel.onmessage = rtc.onMsg;
-        receiveChannel.onopen = rtc.openDataChannel;
-        receiveChannel.onclose = rtc.closeDataChannel;
-    }
-};
-
-var video = {
-    stream: null,
-    get: function(){
-        if(navigator.getUserMedia){
-            navigator.getUserMedia({video: true, audio: false}, function(stream){
-                video.stream = stream;
-                video.remoteStream(stream);
-            }, console.log);
-        } else { console.log('Telepresence, not supported on this device'); }
-    },
-    remoteStream: function(event){
-        document.getElementById('remoteVid').srcObject = event;
-        // document.getElementById('remoteVid').src = window.URL.createObjectURL(event.stream);
-    }
-};
-
-var connect = {
-    friend: function(){
-        console.log('trying to find you a friend so u no forever lonely');
+        receiveChannel.onerror = function onError(){};                         // handling errors could be a good idea
+        receiveChannel.onmessage = function onMsg(event){
+            app.appendMsg('Peer: ' + event.data);
+        };
+        receiveChannel.onopen = function onOpen(){rtc.channelOpen = true;};    // how one would handle events upon opening connection
+        receiveChannel.onclose = function onClose(){rtc.channelOpen = false;}; // how one would handle events upon closing connection
     }
 };
 
 var app = {
+    receiveBox: document.getElementById('receiveBox'),
+    sendBox: document.getElementById('sendBox'),
+    connectInput: document.getElementById('offerAnswerInput'),
+    resultsField: document.getElementById('offerAnswerResults'),
     init: function(){
         document.addEventListener('DOMContentLoaded', function(){
-            // video.get();
             rtc.connectInit();
         });
+    },
+    sendMsg: function(){
+        rtc.sendData(app.sendBox.value);
+        app.appendMsg('Me  : ' + app.sendBox.value);
+        app.sendBox.value = '';
+    },
+    appendMsg: function(msg){
+        var line = document.createElement("p");
+        var txtNode = document.createTextNode(msg);
+        line.appendChild(txtNode);
+        app.receiveBox.appendChild(line);
     }
 };
 
