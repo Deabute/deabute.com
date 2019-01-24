415b1b5908529a1db05087d3f40366765f9a6dc9
PaulBeaudet <inof8or@gmail.com>
2019-01-24 15:12:15 -0500

    store nick locally

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -24,22 +24,16 @@ var rtc = { // stun servers in config allow client to introspect a communication
         rtc.peer.createOffer(offerConfig).then( function onOffer(desc){       // get sdp data to show user, that will share with a friend
             return rtc.peer.setLocalDescription(desc);                        // note what sdp data self will use
         }).then( function onSet(){
-            console.log('sending offer');
             ws.send({type: 'offer', sdp: rtc.peer.localDescription, friendName: friendName}); // send offer to friend
         });
     },
-    onOffer: function(sdp){
+    giveAnswer: function(sdp){
         rtc.peer.setRemoteDescription(sdp);
         rtc.peer.createAnswer().then(function onAnswer(answer){ // create answer to remote peer that offered
             return rtc.peer.setLocalDescription(answer);        // set that offer as our local discripion
         }).then(function onOfferSetDesc(){
-            console.log('sending answer');
             ws.send({type: 'answer', sdp: rtc.peer.localDescription, friendId: ws.friendId}); // send offer to friend
         });                                                     // note answer is shown to user in onicecandidate event above once resolved
-    },
-    connect: function(){                                        // what to do when a friends session id is entered
-        rtc.init(function(){rtc.createOffer(app.nameInput.value);});
-        app.nameInput.value = '';
     }
 };
 
@@ -49,29 +43,27 @@ var dataPeer = {
     newChannel: function(event){
         receiveChannel = event.channel;                          // recieve channel events handlers created on connection
         receiveChannel.onerror = function onError(){};           // handling errors could be a good idea
-        receiveChannel.onmessage = function onMsg(event){
-            var res = dataPeer.incoming(event.data);
-            if(res.type){ws.send(res);}
-        };
+        receiveChannel.onmessage = dataPeer.incoming;
         receiveChannel.onopen = function onOpen(){
             dataPeer.connected = true;
             dataPeer.send({type: 'connect'});
         };    // handle events upon opening connection
         receiveChannel.onclose = function onClose(){dataPeer.connected = false;}; // doenst seem to work on closing a tab
     },
-    incoming: function(message){               // handle incoming socket messages
+    incoming: function(event){                 // handle incoming socket messages
         var req = {type: null};                // request
-        try {req = JSON.parse(message);}       // probably should be wrapped in error handler
-        catch(error){}                         // if error we don't care there is a default object
+        try {req = JSON.parse(event.data);}catch(error){}       // probably should be wrapped in error handler
         var res = {type: null};                // response
         if(req.type === 'disconnect'){
-            app.changeMode(1);                 // jump to startin point
+            app.showConnect();                 // show ability to connect once disconnected
         } else if(req.type === 'connect'){
-            app.changeMode(4);                 // last stage mode change
-        } else {
-            console.log(message);              // will log message regardless of whether it was parsed
-        }
-        return res;
+            app.friendInput.value = '';
+            app.friendInput.style.visibility = 'hidden';
+            app.discription.innerHTML = 'connected';
+            app.connectButton.innerHTML = 'Disconnect';
+            app.connectButton.style.visibility = 'visible';
+        } // else { console.log(event.data); }        // will log message regardless of whether it was parsed
+        if(res.type){dataPeer.send(res);}
     },
     send: function(sendObj){
         try{sendObj = JSON.stringify(sendObj);} catch(error){console.log(error);}
@@ -86,7 +78,6 @@ var dataPeer = {
 };
 
 var ws = {
-    id: null,          // id of this connection set by server
     friendId: '',      // socket id of peer connection
     instance: null,    // placeholder for websocket object
     connected: false,  // set to true when connected to server
@@ -94,34 +85,29 @@ var ws = {
         ws.instance = new WebSocket(server);
         ws.instance.onopen = function(event){
             ws.connected = true;
-            ws.instance.onmessage = function(event){
-                var res = ws.incoming(event.data);
-                if(res.type){ws.send(res);}
-            };
+            ws.instance.onmessage = ws.incoming;
+            ws.send({type: 'connected', username: localStorage.username}); // may not have username, no problem, just need an ack
             ws.onclose = function onSocketClose(){ws.connected = false;};
             ws.onerror = function onSocketError(){console.log(error);};
         };
     },
-    incoming: function(message){         // handle incoming socket messages
+    incoming: function(event){         // handle incoming socket messages
         var req = {type: null};          // request
-        try {req = JSON.parse(message);} // probably should be wrapped in error handler
+        try {req = JSON.parse(event.data);} // probably should be wrapped in error handler
         catch(error){}                   // if error we don't care there is a default object
         var res = {type: null};          // response
-        if(req.type === 'token'){
-            ws.id = req.data;
-        } else if(req.type === 'offer'){
+        if(req.type === 'offer'){
             ws.friendId = req.id;
-            rtc.init(function onInit(){rtc.onOffer(req.sdp);});
+            rtc.init(function onInit(){rtc.giveAnswer(req.sdp);});
         } else if(req.type === 'answer'){
             rtc.peer.setRemoteDescription(req.sdp);
         } else if(req.type === 'ice'){
             rtc.peer.addIceCandidate(req.canidate);
         } else if(req.type === 'nomatch'){
-            console.log('no matches found');
-        } else {
-            // console.log(message);        // will log message regardless of whether it was parsed
-        }
-        return res;
+            app.showConnect();
+            app.discription.innerHTML = 'no soup for you';
+        } // else { console.log(event.data); }    // will log message regardless of whether it was parsed
+        if(res.type){ws.send(res);}
     },
     send: function(msg){
         try{msg = JSON.stringify(msg);} catch(error){console.log(error);}
@@ -142,6 +128,8 @@ var media = {
         var onMediaCallback = onMedia ? onMedia : function noSoupForYou(){};
         navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(function onMedia(mediaStream){
             media.stream = mediaStream;
+            app.setupButton.style.visibility = 'hidden';
+            app.showConnect();
             var audioTracks = mediaStream.getAudioTracks();
             if(audioTracks.length){
                 if(audioTracks[0].enabled){
@@ -153,56 +141,57 @@ var media = {
 };
 
 var app = {
-    chatMode: 1, // Determites if showing talkin mode or connecting mode
     modeButton: document.getElementById('modeButton'),
-    nameInput: document.getElementById('nameInput'),
+    setupInput: document.getElementById('setupInput'),
+    setupButton: document.getElementById('setupButton'),
+    friendInput: document.getElementById('friendInput'),
+    connectButton: document.getElementById('connectButton'),
     discription: document.getElementById('discription'),
     init: function(){
         document.addEventListener('DOMContentLoaded', function(){       // wait till dom is loaded before manipulating it
+            ws.init(document.getElementById('socketserver').innerHTML, localStorage.username, localStorage.uid);
             document.getElementById('socketserver').style.visibility = 'hidden'; // hide, not sure how to do this in html
-            app.modeButton.innerHTML = 'Enter name and allow Microphone';
-            ws.init(document.getElementById('socketserver').innerHTML); // grab socket server from compiled jekyll temlpate for this env
+            app.hideConnect();
+            if(localStorage.username){
+                app.discription.innerHTML = 'Welcome back';
+                app.setupButton.innerHTML = 'Turn on Microphone';
+                app.setupInput.value = localStorage.username;
+            } else {
+                app.setupButton.innerHTML = 'Enter name if you would like';
+            }
         });
     },
-    changeMode: function(chatMode){ // change between chat or connect view
-        app.chatMode = chatMode ? chatMode : app.chatMode + 1;
-        if(app.chatMode > 4){app.chatMode = 0;}
-        if(app.chatMode === 0){
+    setup: function(){
+        app.setupButton.innerHTML = 'Please allow Microphone, in order to connect';
+        if(localStorage.username !== app.setupInput.value){ // create or change username
+            ws.send({type: 'name', name: app.setupInput.value.toLowerCase()});
+            localStorage.username = app.setupInput.value;
+        }
+        app.setupInput.style.visibility = 'hidden';
+        media.init();
+    },
+    showConnect: function(){
+        if(rtc.peer){ rtc.peer.close(); rtc.peer = null;} // clean up pre existing rtc connection if there
+        app.discription.innerHTML = 'Enter a friend\'s name or just press connect to get a match';
+        app.friendInput.style.visibility = 'visible';
+        app.connectButton.innerHTML = 'Connect';
+        app.connectButton.style.visibility = 'visible';
+    },
+    hideConnect: function(){
+        app.friendInput.value = '';
+        app.friendInput.style.visibility = 'hidden';
+        app.connectButton.style.visibility = 'hidden';
+    },
+    toggleConnection: function(){
+        if(dataPeer.connected){
             dataPeer.send({type: 'disconnect'}); // tell friend we are done
             ws.send({type: 'disconnect'});       // tell server we are done
-            app.changeMode(1);                   // jump to starting point
-        } else if(app.chatMode === 1){           // end chat button was press
-            ws.friendId = '';
-            rtc.peer.close();
-            rtc.peer = null;
-            // app.discription.style.visibility = 'visible';
-            if(media.stream){app.changeMode(2);}
-            else { app.modeButton.innerHTML = 'Enter name and allow Microphone'; }
-        } else if(app.chatMode === 2){
-            if(media.stream){
-                app.discription.style.visibility = 'visible';
-                app.nameInput.style.visibility = 'visible';
-            } else {
-                media.init();
-                ws.send({type: 'name', name: app.nameInput.value});
-            }
-            app.nameInput.value = '';
-            app.modeButton.innerHTML = 'Connect (empty for whomever)';
-        } else if(app.chatMode == 3){
-            if(media.stream){
-                rtc.connect();
-                app.modeButton.style.visibility = 'hidden';
-            } else {
-                app.modeButton.innerHTML = 'No voice detected';
-                media.init();
-                app.chatMode = 2;
-            }
-        } else if (app.chatMode === 4){
-            app.modeButton.style.visibility = 'visible';
-            app.nameInput.style.visibility = 'hidden';
-            app.discription.style.visibility = 'hidden';
-            app.modeButton.innerHTML =  'disconnect';
-        } else {console.log('ya daw gone messed up');}
+            app.showConnect();
+        } else {
+            rtc.init(function(){rtc.createOffer(app.friendInput.value.toLowerCase());});
+            app.discription.innerHTML = 'connecting';
+            app.hideConnect();
+        }
     }
 };
 
