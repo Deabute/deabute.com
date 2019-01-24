ac54ad55413b01119297d1642bd713648fd3f418
PaulBeaudet <inof8or@gmail.com>
2019-01-24 17:15:50 -0500

    include questions script

---
+++ assets/js/questions.js
@@ -0,0 +1,14 @@
+var affinity = [
+    {
+        question: 'Are you into the outdoors?',
+        answer:['modern day caveperson', 'nah', 'meh', 'its nice to get out', 'Can\'t get enough']
+    },
+    {
+        question: 'Do you consider yourself a cretive person?',
+        answer:[ 'definitely not', 'no','meh', 'would like to be', 'definitely', ]
+    },
+    {
+        question: 'Do you have pets?',
+        answer: ['yes', 'no']
+    }
+];
