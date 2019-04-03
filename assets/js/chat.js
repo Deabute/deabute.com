9e7c154c6e28c1daddaa7efd62f7af620764af4a
PaulBeaudet <inof8or@gmail.com>
2019-04-03 16:22:15 -0400

    turn back time

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -335,7 +335,7 @@ var persistence = {
 };
 
 var DAY_OF_WEEK = 3;
-var HOUR_OF_DAY = 15;
+var HOUR_OF_DAY = 16;
 var CONSENT_MINUTE = 11;
 var OPEN_MINUTE = CONSENT_MINUTE - 10;
 var CONFLUENCE_MINUTE = CONSENT_MINUTE;
